d940181ab57f0082a4a364806ccb6f2e7916b02b
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-11 21:24:05 +0200

    Update FCFS Remove Comments

--- fcfs.cpp
+++ fcfs.cpp
@@ -1,14 +1,12 @@
 #include "fcfs.hpp"
-bool fcfs_sort(Process *p1, Process *p2)
-{
+bool fcfs_sort(Process *p1, Process *p2) {
   int t1 = p1->arrive_time;
   int t2 = p2->arrive_time;
 
   return t1 < t2;
 }
 
-void first_come_first_serve(std::vector<Process *> processes)
-{
+void first_come_first_serve(std::vector<Process *> processes) {
   using namespace std;
   int t = 0;
   cout << "In FCFS\n";
@@ -16,24 +14,18 @@ void first_come_first_serve(std::vector<Process *> processes)
   queue<Process *> q;
 
   std::sort(processes.begin(), processes.end(), &fcfs_sort);
+
   for (Process *p : processes)
     q.push(p);
-  while (!q.empty())
-  {
+
+  while (!q.empty()) {
     Process *p = q.front();
     q.pop();
-    std::cout << p->name << "\t";
-    while (p->remaining_time != 0)
-    {
+    while (p->remaining_time != 0) {
       p->remaining_time--;
       p->status[t] = '*';
       t++;
     }
-    // std::cout << p->status.size();
-    for (char x : p->status)
-      std::cout << x << " ";
-    cout << endl;
   }
-
   std::cout << std::endl;
 }
