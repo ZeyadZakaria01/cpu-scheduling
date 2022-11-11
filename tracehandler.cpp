ce60108cc93b4ee70125a081dec910d69f7149aa
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-11 20:24:28 +0200

    Update tracehandler.cpp

--- tracehandler.cpp
+++ tracehandler.cpp
@@ -17,41 +17,24 @@ void TraceHandler::print_trace(std::vector<Process *> simulation, string mode) {
     cout << i % 10 << " ";
   }
   // second row
-  cout << "------------------------------------------------";
+  cout << "\n------------------------------------------------\n";
   //
   for (Process *p : simulation) {
-    int t = this->last_instances;
+    int t = 0;
     int r = p->service_time;
     cout << p->name << "     ";
     for (char ch : p->status) {
       cout << "|";
-      if (p->arrive_time > t && ch != '*' && r != 0) {
+      if (p->arrive_time <= t && ch != '*' ) {
         cout << ".";
       } else {
-        if (ch == '*')
-          r--;
         cout << ch;
       }
-      cout << "|";
+      t++;
     }
+      cout << "|";
     cout << "\n"; // TODO:maybe better to flush to use endl
   }
 
-  cout << "------------------------------------------------";
-}
-
-int main() {
-  vector<Process *> simulation;
-  string mode = "FCFS";
-  int last_instances = 20;
-  int n = 5;
-  for (int i = 0; i < 5; i++) {
-    char ch;
-    int x, y;
-    Process *p = new Process(ch, x, y, last_instances);
-    simulation.push_back(p);
-  }
-  first_come_first_serve(simulation);
-  TraceHandler *tc = new TraceHandler(last_instances);
-  tc->print_trace(simulation, mode);
+  cout << "------------------------------------------------\n";
 }
