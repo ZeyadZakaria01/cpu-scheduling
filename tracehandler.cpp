0f274745eafb1bf3924b5068d0f29f205862be41
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-11 21:43:04 +0200

    Update tracehandler.cpp made second row dynamic

--- tracehandler.cpp
+++ tracehandler.cpp
@@ -11,14 +11,17 @@ TraceHandler::TraceHandler(int last_instances) {
 }
 
 void TraceHandler::print_trace(std::vector<Process *> simulation, string mode) {
+  int l = mode.length()+4 + 2 * last_instances;
   // first row
   cout << mode << "  ";
   for (int i = 0; i < this->last_instances + 1; i++) {
     cout << i % 10 << " ";
   }
   // second row
-  cout << "\n------------------------------------------------\n";
-  //
+  cout << endl;
+  for (int i = 0; i < l; i++)
+    cout << '-';
+  cout << endl;
   for (Process *p : simulation) {
     int t = 0;
     int r = p->service_time;
@@ -42,5 +45,8 @@ void TraceHandler::print_trace(std::vector<Process *> simulation, string mode) {
     cout << "\n"; // TODO:maybe better to flush to use endl
   }
 
-  cout << "------------------------------------------------\n";
+  // second row
+  for (int i = 0; i < l; i++)
+    cout << '-';
+  cout << endl;
 }
