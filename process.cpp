da60c3ced7ccd1463ded4759de57286516449486
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-11 17:44:59 +0200

    Update Process Class

--- process.cpp
+++ process.cpp
@@ -1,12 +1,13 @@
-#include <bits/stdc++.h>
 
 #include "process.hpp"
 
-Process::Process(char name, int arrive_time, int service_time) {
+Process::Process(char name, int arrive_time, int service_time,
+                 int last_instant) {
   this->name = name;
   this->arrive_time = arrive_time;
   this->service_time = service_time;
   this->remaining_time = service_time;
+  this->status = std::vector<char>(last_instant, ' ');
 }
 
 Process::Process() {}
--- process.hpp
+++ process.hpp
@@ -1,5 +1,6 @@
 #ifndef process_hpp
 #define process_hpp
+#include <bits/stdc++.h>
 
 class Process {
 public:
@@ -8,9 +9,10 @@ public:
   int service_time;
   int remaining_time;
   int finish_time;
+  std::vector<char> status;
 
   Process();
-  Process(char name,int arrive_time,int service_time);
+  Process(char name, int arrive_time, int service_time,int last_instant);
   float getNormTurn();
   int getTurnAround();
 };
