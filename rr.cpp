16bd3880751b019b003d12872b1eaf4c46e47418
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-11-12 01:01:42 +0200

    Added quantum to Round Roubin!

--- rr.cpp
+++ rr.cpp
@@ -1,44 +1,47 @@
 #include "rr.hpp"
-
+bool check_arrived(std::vector<Process *> processes, std::queue<Process *> &q, int t)
+{
+    bool flag = false;
+    for (Process *p : processes)
+    {
+        if (p->arrive_time == t)
+        {
+            q.push(p);
+            flag = true;
+        }
+    }
+    return flag;
+}
 void rr_serve(std::vector<Process *> processes, int quantum, int last_instant)
 {
     using namespace std;
     int t = 0;
     cout << "In RR\n";
     queue<Process *> q;
-    for (Process *p : processes)
-    {
-        if (p->arrive_time == 0)
-            q.push(p);
-    }
+    bool nothing_arrived = true;
     while (t < last_instant)
     {
-
+        if (nothing_arrived)
+            nothing_arrived = !check_arrived(processes, q, t);
+        int st_time = t;
         Process *p = q.front();
-        q.pop();
         p->status[t] = '*';
         p->remaining_time--;
+        q.pop();
         t++;
-        for (Process *p : processes)
+        check_arrived(processes, q, t);
+        int qu = quantum;
+        while (--qu && p->remaining_time > 0)
         {
-            if (t == p->arrive_time)
-                q.push(p);
-            else if (t < p->arrive_time)
-                break;
+            cout << "how?" << endl;
+            p->status[t] = '*';
+            p->remaining_time--;
+            t++;
+            check_arrived(processes, q, t);
         }
         if (p->remaining_time > 0)
         {
             q.push(p);
         }
     }
-    for (Process *p : processes)
-    {
-        std::cout << p->name << "\t";
-        std::cout << "|";
-        for (char x : p->status)
-            std::cout << x << "|";
-        cout << endl;
-    }
-
-    std::cout << std::endl;
 }
