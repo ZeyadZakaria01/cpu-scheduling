49141f2291cefc7e6aa477b568c04110c679de32
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-11 17:45:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct process
-{
-    char name;
-    int arrive;
-    int service_time;
-};
+#include "fcfs.hpp"
+#include "process.hpp"
+#include <cwchar>
+#include <vector>
 
-int main()
-{
-    vector<string> vc(4);
-    for (int i = 0; i < 4; i++)
-    {
-        cin >> vc[i];
-    }
-    string mode = vc[0];
-    string policy = vc[1];
-    int last_instant = stoi(vc[2]);
-    int number_of_processes = stoi(vc[3]);
-    vector<process> processes(number_of_processes);
-    for (int i = 0; i < number_of_processes; i++)
-    {
-        string str;
-        cin >> str;
-        std::replace(str.begin(), str.end(), ',', ' '); // replace ':' by ' '
-        vector<string> array;
-        stringstream ss(str);
-        string temp;
-        while (ss >> temp)
-            array.push_back(temp);
-        processes[i].name = array[0][0];
-        processes[i].arrive = stoi(array[1]);
-        processes[i].service_time = stoi(array[2]);
-    }
-    cout << "mode: " << mode << endl;
-    cout << "policy: " << policy << endl;
-    printf("last_instant: %d\nnumber_of_processes:%d\n", last_instant, number_of_processes);
-    for (int i = 0; i < number_of_processes; i++)
-    {
-        printf("%c,%d,%d\n", processes[i].name, processes[i].arrive, processes[i].service_time);
-    }
-    return 0;
+using namespace std;
+
+void do_scheduling(string str, vector<Process *> processes) {
+  // TODO: add forloop in case of more than one policy
+  int policy = stoi(str);
+  switch (policy) {
+  case 1: {
+    first_come_first_serve(processes);
+    break;
+  }
+  default: {
+    cout << "Random value I guess? :D";
+  }
+  }
+}
+
+int main() {
+  vector<string> vc(4);
+  for (int i = 0; i < 4; i++) {
+    cin >> vc[i];
+  }
+  string mode = vc[0];
+  string policy = vc[1];
+  int last_instant = stoi(vc[2]);
+  int number_of_processes = stoi(vc[3]);
+  vector<Process *> processes;
+  for (int i = 0; i < number_of_processes; i++) {
+    string str;
+    cin >> str;
+    std::replace(str.begin(), str.end(), ',', ' '); // replace ',' by ' '
+    vector<string> array;
+    stringstream ss(str);
+    string temp;
+    while (ss >> temp)
+      array.push_back(temp);
+    Process *p =
+        new Process(array[0][0], stoi(array[1]), stoi(array[2]), last_instant);
+    processes.push_back(p);
+  }
+  cout << "mode: " << mode << endl;
+  cout << "policy: " << policy << endl;
+  printf("last_instant: %d\nnumber_of_processes:%d\n", last_instant,
+         number_of_processes);
+  for (int i = 0; i < number_of_processes; i++) {
+    printf("%c,%d,%d\n", processes[i]->name, processes[i]->arrive_time,
+           processes[i]->service_time);
+  }
+  do_scheduling(policy, processes);
+
+  return 0;
 }
