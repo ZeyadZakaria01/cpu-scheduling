735e288da064f6c8a7acfe55f654704c61f9ad58
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-11-13 16:16:50 +0200

    Adding check_arrival to header and delete cout

--- rr.hpp
+++ rr.hpp
@@ -3,6 +3,7 @@
 
 #include "process.hpp"
 
+bool check_arrived(std::vector<Process *> processes, std::queue<Process *> &q, int t);
 void rr_serve(std::vector<Process *> processes, int quantum, int last_instant);
 
 #endif
