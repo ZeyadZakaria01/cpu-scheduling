67b7158f6721113fd96a33d6229d9f98bbf6c141
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-13 13:24:52 +0200

    Add Both Handlers

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "rr.hpp"
 #include "spn.hpp"
 #include "srt.hpp"
+#include "statshandler.hpp"
 #include "tracehandler.hpp"
 
 using namespace std;
@@ -18,8 +19,10 @@ vector<Process *> deep_copy(vector<Process *> vc) {
   return new_vc;
 }
 
-void do_scheduling(string str, vector<Process *> processes, int last_instant) {
-  TraceHandler *tc = new TraceHandler(last_instant);
+void do_scheduling_trace(string str, vector<Process *> processes,
+                         int last_instant) {
+
+  TraceHandler *handler = new TraceHandler(last_instant);
 
   std::replace(str.begin(), str.end(), ',', ' ');
   vector<string> array;
@@ -46,40 +49,112 @@ void do_scheduling(string str, vector<Process *> processes, int last_instant) {
     switch (policy) {
     case 1: {
       first_come_first_serve(p_copy);
-      tc->print_trace(p_copy, "FCFS");
+      handler->output(p_copy, "FCFS");
       break;
     }
     case 2: {
       rr_serve(p_copy, quantum, last_instant);
       string str = "RR-" + to_string(quantum);
-      tc->print_trace(p_copy, str);
+      handler->output(p_copy, str);
       break;
     }
     case 3: {
       shortest_process_next(p_copy, last_instant);
-      tc->print_trace(p_copy, "SPN");
+      handler->output(p_copy, "SPN");
       break;
     }
     case 4: {
       srt(p_copy, last_instant);
-      tc->print_trace(p_copy, "SRT");
+      handler->output(p_copy, "SRT");
       break;
     }
     case 5: {
       hrrn(p_copy, last_instant);
-      tc->print_trace(p_copy, "HRRN");
+      handler->output(p_copy, "HRRN");
       break;
     }
     case 6: {
-      tc->print_trace(p_copy, "FB-1");
+      handler->output(p_copy, "FB-1");
       break;
     }
     case 7: {
-      tc->print_trace(p_copy, "FB-2i");
+      handler->output(p_copy, "FB-2i");
       break;
     }
     case 8: {
-      tc->print_trace(p_copy, "Aging");
+      handler->output(p_copy, "Aging");
+      break;
+    }
+    default:
+      cout << "Random value I guess? :D";
+    }
+  }
+}
+
+void do_scheduling_stats(string str, vector<Process *> processes,
+                         int last_instant) {
+
+  StatsHandler *handler = new StatsHandler(last_instant);
+
+  std::replace(str.begin(), str.end(), ',', ' ');
+  vector<string> array;
+  stringstream ss(str);
+  string q_policy;
+  while (ss >> q_policy) {
+    vector<Process *> p_copy = deep_copy(processes);
+    // vector<Process *> p_copy = processes;
+
+    int policy;
+    int quantum;
+    std::replace(q_policy.begin(), q_policy.end(), '-', ' ');
+    stringstream my_ss(q_policy);
+    string temp;
+    bool is_policy = true;
+    while (my_ss >> temp) {
+      if (is_policy) {
+        policy = stoi(temp);
+        is_policy = false;
+      } else {
+        quantum = stoi(temp);
+      }
+    }
+    switch (policy) {
+    case 1: {
+      first_come_first_serve(p_copy);
+      handler->output(p_copy, "FCFS");
+      break;
+    }
+    case 2: {
+      rr_serve(p_copy, quantum, last_instant);
+      string str = "RR-" + to_string(quantum);
+      handler->output(p_copy, str);
+      break;
+    }
+    case 3: {
+      shortest_process_next(p_copy, last_instant);
+      handler->output(p_copy, "SPN");
+      break;
+    }
+    case 4: {
+      srt(p_copy, last_instant);
+      handler->output(p_copy, "SRT");
+      break;
+    }
+    case 5: {
+      hrrn(p_copy, last_instant);
+      handler->output(p_copy, "HRRN");
+      break;
+    }
+    case 6: {
+      handler->output(p_copy, "FB-1");
+      break;
+    }
+    case 7: {
+      handler->output(p_copy, "FB-2i");
+      break;
+    }
+    case 8: {
+      handler->output(p_copy, "Aging");
       break;
     }
     default:
@@ -119,7 +194,8 @@ int main() {
     printf("%c,%d,%d\n", processes[i]->name, processes[i]->arrive_time,
            processes[i]->service_time);
   }
-  do_scheduling(policy, processes, last_instant);
+  mode == "stats" ? do_scheduling_stats(policy, processes, last_instant)
+                  : do_scheduling_trace(policy, processes, last_instant);
 
   return 0;
 }
