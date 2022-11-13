f052f50dc9c5ed458eb3765e6998999b308827d9
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-13 06:44:02 +0200

    Add Waiting time attribute

--- process.cpp
+++ process.cpp
@@ -9,6 +9,15 @@ Process::Process(char name, int arrive_time, int service_time,
   this->remaining_time = service_time;
   this->status = std::vector<char>(last_instant, ' ');
 }
+Process::Process(char name, int arrive_time, int service_time,
+                 int last_instant,int waiting_time) {
+  this->name = name;
+  this->arrive_time = arrive_time;
+  this->service_time = service_time;
+  this->remaining_time = service_time;
+  this->waiting_time= waiting_time;
+  this->status = std::vector<char>(last_instant, ' ');
+}
 
 Process::Process() {}
 
--- process.hpp
+++ process.hpp
@@ -9,10 +9,12 @@ public:
   int service_time;
   int remaining_time;
   int finish_time;
+  int waiting_time;
   std::vector<char> status;
 
   Process();
   Process(char name, int arrive_time, int service_time,int last_instant);
+  Process(char name, int arrive_time, int service_time,int last_instant,int waiting_time);
   float getNormTurn();
   int getTurnAround();
 };
