0a2b12b60a404b5e7210313b323b241b1411a73d
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-13 06:44:18 +0200

    Update HRRN

--- hrrn.cpp
+++ hrrn.cpp
@@ -1,17 +1,24 @@
 #include "hrrn.hpp"
+#include "process.hpp"
+#include <complex>
 
 int t;
 
 bool hrrn_priority(Process *p1, Process *p2) {
-  int w1 = t - p1->arrive_time - (p1->service_time - p1->remaining_time);
+  /* int w1 = t - p1->arrive_time - (p1->service_time - p1->remaining_time); */
   // 13 - 6 - ( 5 - 5) = 7
-  int w2 = t - p2->arrive_time - (p2->service_time - p2->remaining_time);
+  /* int w2 = t - p2->arrive_time - (p2->service_time - p2->remaining_time); */
   // 13 - 8 - (2 - 2) = 5
   //
+  int w1 = p1->waiting_time;
+  int w2 = p2->waiting_time;
   // 7 +5 / 5 = 12 / 5 = 2.x
   //  5+ 2 / 5 = 7/5 =  1.x
   float r1 = (w1 + p1->service_time) / float(p1->service_time);
   float r2 = (w2 + p2->service_time) / float(p2->service_time);
+  /* printf("at t3 = %d, %c-> %f, w1 = %d - %c-> %f, w2 = %d\n", t, p1->name,
+   * r1, */
+  /*        w1, p2->name, r2, w2); */
 
   return r1 < r2;
 }
@@ -24,6 +31,7 @@ void hrrn(std::vector<Process *> processes, int last_instant) {
       pq(hrrn_priority);
 
   while (t < last_instant) {
+
     for (Process *p : processes) {
       if (p->arrive_time == t) {
         pq.push(p);
@@ -44,6 +52,15 @@ void hrrn(std::vector<Process *> processes, int last_instant) {
       p->remaining_time--;
       p->status[t] = '*';
       t++;
+      vector<Process *> waiting_in_pq;
+      while (!pq.empty()) {
+        Process *p_waiting = pq.top();
+        pq.pop();
+        p_waiting->waiting_time++;
+        waiting_in_pq.push_back(p_waiting);
+      }
+      for (auto waiting_proces: waiting_in_pq)
+        pq.push(waiting_proces);
     }
   }
 }
