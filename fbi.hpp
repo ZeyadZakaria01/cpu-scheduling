395016d8b966c19c000f251d434f7049ef65b103
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-11-13 19:19:38 +0200

    Adding Feedback Policy with q=2^i

---
+++ fbi.hpp
@@ -0,0 +1,10 @@
+#ifndef fbi_hpp
+#define fbi_hpp
+
+#include "process.hpp"
+#include "utils.hpp"
+#include <math.h>
+
+void fbi(std::vector<Process *> processes, int last_instant, int number_of_queues);
+
+#endif
