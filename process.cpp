179ee734e8f5c7702ff00b54a2da2465b5bb5624
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-11-13 19:24:40 +0200

    Initializing finish_time & wait_time in constructor

--- process.cpp
+++ process.cpp
@@ -1,32 +1,27 @@
 
 #include "process.hpp"
 
-Process::Process(char name, int arrive_time, int service_time,
-                 int last_instant) {
+Process::Process(char name, int arrive_time, int service_time, int last_instant)
+{
   this->name = name;
   this->arrive_time = arrive_time;
   this->service_time = service_time;
   this->remaining_time = service_time;
   this->status = std::vector<char>(last_instant, ' ');
-}
-Process::Process(char name, int arrive_time, int service_time,
-                 int last_instant,int waiting_time) {
-  this->name = name;
-  this->arrive_time = arrive_time;
-  this->service_time = service_time;
-  this->remaining_time = service_time;
-  this->waiting_time= waiting_time;
-  this->status = std::vector<char>(last_instant, ' ');
+  this->waiting_time = 0;
+  this->finish_time = 0;
 }
 
 Process::Process() {}
 
-int Process::getTurnAround() {
+int Process::getTurnAround()
+{
 
   int res = this->finish_time - this->arrive_time;
   return res;
 }
 
-float Process::getNormTurn() {
+float Process::getNormTurn()
+{
   return this->getTurnAround() / (float)this->service_time;
 }
