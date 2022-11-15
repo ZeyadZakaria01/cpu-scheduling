e8588469510c1f657cee1b1e33eea6e7242db5c5
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-15 18:29:27 +0200

    Add Aging Skeleton

---
+++ aging.cpp
@@ -0,0 +1,10 @@
+#include "aging.hpp"
+
+using namespace std;
+
+void aging(vector<Process *> processes, int quantum, int last_instant) {
+  //
+  //
+  //
+}
+
---
+++ aging.hpp
@@ -0,0 +1,8 @@
+#ifndef aging_hpp
+#define agin_hpp
+
+#include "process.hpp"
+
+void aging(std::vector<Process *> processes, int quantum, int last_instant);
+
+#endif
