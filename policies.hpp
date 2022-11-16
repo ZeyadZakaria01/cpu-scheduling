41a226ccc3ae1ebd46521ae2cd1cd75aca25f7d8
iZarrios <eyad_zarrios2001@yahoo.com>
2022-11-16 20:29:11 +0200

    Add policies header file

---
+++ policies.hpp
@@ -0,0 +1,13 @@
+#ifndef POLICIES_HPP
+#define POLICIES_HPP
+
+#include "fcfs.hpp"
+#include "rr.hpp"
+#include "spn.hpp"
+#include "srt.hpp"
+#include "hrrn.hpp"
+#include "fb1.hpp"
+#include "fbi.hpp"
+#include "aging.hpp"
+
+#endif
